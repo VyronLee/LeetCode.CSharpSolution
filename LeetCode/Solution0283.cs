cedf041435381a1a865e09783271a7f51853d730
VyronLee <lwz_jz@hotmail.com>
2024-02-17 21:53:07 +0800

    Add question 283 solution.

---
+++ LeetCode/Solution0283.cs
@@ -0,0 +1,64 @@
+﻿// ------------------------------------------------------------
+//         File: Solution283.cs
+//        Brief: Solution283.cs
+//
+//       Author: VyronLee, lwz_jz@hotmail.com
+//
+//      Created: 2024-2-17 21:30
+//    Copyright: Copyright (c) 2024, VyronLee
+// ============================================================
+
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+//
+// 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入: nums = [0,1,0,3,12]
+//输出: [1,3,12,0,0]
+//
+//
+// 示例 2:
+//
+//
+//输入: nums = [0]
+//输出: [0]
+//
+//
+//
+// 提示:
+//
+//
+//
+// 1 <= nums.length <= 10⁴
+// -2³¹ <= nums[i] <= 2³¹ - 1
+//
+//
+//
+//
+// 进阶：你能尽量减少完成的操作次数吗？
+//
+// Related Topics 数组 双指针 👍 2301 👎 0
+
+using System;
+
+namespace LeetCode
+{
+    public class Solution283
+    {
+        public void MoveZeroes(int[] nums) {
+            for (int i = 0, j = nums.Length; i < nums.Length; i++) {
+                if (nums[i] == 0) {
+                    j = Math.Min(i, j);
+                }
+                else if(i > j) {
+                    (nums[i], nums[j]) = (nums[j], nums[i]);
+                    j++;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 7. [x] [238. 除自身以外数组的乘积](https://leetcode.cn/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75)
 8. [x] [334. 递增的三元子序列](https://leetcode.cn/problems/increasing-triplet-subsequence/description/?envType=study-plan-v2&envId=leetcode-75)
 9. [x] [443. 压缩字符串](https://leetcode.cn/problems/string-compression/description/?envType=study-plan-v2&envId=leetcode-75)
-10. [ ] [283. 移动零](https://leetcode.cn/problems/move-zeroes/description/?envType=study-plan-v2&envId=leetcode-75)
+10. [x] [283. 移动零](https://leetcode.cn/problems/move-zeroes/description/?envType=study-plan-v2&envId=leetcode-75)
 11. [ ] [392. 判断子序列](https://leetcode.cn/problems/is-subsequence/description/?envType=study-plan-v2&envId=leetcode-75)
 12. [ ] [11. 盛最多水的容器](https://leetcode.cn/problems/container-with-most-water/description/?envType=study-plan-v2&envId=leetcode-75)
 13. [ ] [1679. K 和数对的最大数目](https://leetcode.cn/problems/max-number-of-k-sum-pairs/description/?envType=study-plan-v2&envId=leetcode-75)
