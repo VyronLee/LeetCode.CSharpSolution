6f127e892a0cea960aba98dfc825b733035f5bb6
VyronLee <lwz_jz@hotmail.com>
2024-02-28 22:23:25 +0800

    Add question 1161 solution.

---
+++ LeetCode/1001-1500/Solution1161.cs
@@ -0,0 +1,90 @@
+﻿// ------------------------------------------------------------
+//         File: Solution1161.cs
+//        Brief: Solution1161.cs
+//
+//       Author: VyronLee, lwz_jz@hotmail.com
+//
+//      Created: 2024-2-28 22:2
+//    Copyright: Copyright (c) 2024, VyronLee
+// ============================================================
+
+//给你一个二叉树的根节点 root。设根节点位于二叉树的第 1 层，而根节点的子节点位于第 2 层，依此类推。
+//
+// 请返回层内元素之和 最大 的那几层（可能只有一层）的层号，并返回其中 最小 的那个。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：root = [1,7,0,7,-8,null,null]
+//输出：2
+//解释：
+//第 1 层各元素之和为 1，
+//第 2 层各元素之和为 7 + 0 = 7，
+//第 3 层各元素之和为 7 + -8 = -1，
+//所以我们返回第 2 层的层号，它的层内元素之和最大。
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [989,null,10250,98693,-89388,null,null,null,-32127]
+//输出：2
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中的节点数在
+// [1, 10⁴]范围内
+//
+// -10⁵ <= Node.val <= 10⁵
+//
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 131 👎 0
+
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Solution1161
+    {
+        public int MaxLevelSum(TreeNode root) {
+            var (max, depth, maxDepth) = (int.MinValue, 1, 1);
+            var queue1 = new List<TreeNode>(1000);
+            var queue2 = new List<TreeNode>(1000);
+            queue1.Add(root);
+            while (queue1.Count > 0) {
+                var sum = 0;
+                for (var i = 0; i < queue1.Count; i++) {
+                    sum += queue1[i].val;
+                    queue2.Add(queue1[i]);
+                }
+                if (sum > max) {
+                    max = sum;
+                    maxDepth = depth;
+                }
+
+                queue1.Clear();
+                for (var i = 0; i < queue2.Count; i++) {
+                    var treeNode = queue2[i];
+                    if (treeNode.left != null) {
+                        queue1.Add(treeNode.left);
+                    }
+                    if (treeNode.right != null) {
+                        queue1.Add(treeNode.right);
+                    }
+                }
+                queue2.Clear();
+
+                depth++;
+            }
+            return maxDepth;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -57,7 +57,7 @@
 37. [x] [1372. 二叉树中的最长交错路径](https://leetcode.cn/problems/longest-zigzag-path-in-a-binary-tree/?envType=study-plan-v2&envId=leetcode-75)
 38. [x] [236. 二叉树的最近公共祖先](https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/?envType=study-plan-v2&envId=leetcode-75)
 39. [x] [199. 二叉树的右视图](https://leetcode.cn/problems/binary-tree-right-side-view/?envType=study-plan-v2&envId=leetcode-75)
-40. [ ] [1161. 最大层内元素和](https://leetcode.cn/problems/maximum-level-sum-of-a-binary-tree/?envType=study-plan-v2&envId=leetcode-75)
+40. [x] [1161. 最大层内元素和](https://leetcode.cn/problems/maximum-level-sum-of-a-binary-tree/?envType=study-plan-v2&envId=leetcode-75)
 41. [ ] [700. 二叉搜索树中的搜索](https://leetcode.cn/problems/search-in-a-binary-search-tree/?envType=study-plan-v2&envId=leetcode-75)
 42. [ ] [450. 删除二叉搜索树中的节点](https://leetcode.cn/problems/delete-node-in-a-bst/?envType=study-plan-v2&envId=leetcode-75)
 43. [ ] [841. 钥匙和房间](https://leetcode.cn/problems/keys-and-rooms/?envType=study-plan-v2&envId=leetcode-75)
