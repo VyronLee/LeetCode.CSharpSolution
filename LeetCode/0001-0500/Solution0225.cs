be10733b00a4f7003e9dcabb30c678c7a85c85aa
VyronLee <lwz_jz@hotmail.com>
2024-03-03 21:59:35 +0800

    Add question 225 solution.

---
+++ LeetCode/0001-0500/Solution0225.cs
@@ -0,0 +1,112 @@
+﻿// ------------------------------------------------------------
+//         File: Solution0225.cs
+//        Brief: Solution0225.cs
+//
+//       Author: VyronLee, lwz_jz@hotmail.com
+//
+//      Created: 2024-3-3 21:50
+//    Copyright: Copyright (c) 2024, VyronLee
+// ============================================================
+
+//请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+//
+// 实现 MyStack 类：
+//
+//
+// void push(int x) 将元素 x 压入栈顶。
+// int pop() 移除并返回栈顶元素。
+// int top() 返回栈顶元素。
+// boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+//
+//
+//
+//
+// 注意：
+//
+//
+// 你只能使用队列的基本操作 —— 也就是 push to back、peek/pop from front、size 和 is empty 这些操作。
+// 你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+//
+//
+//
+//
+// 示例：
+//
+//
+//输入：
+//["MyStack", "push", "push", "top", "pop", "empty"]
+//[[], [1], [2], [], [], []]
+//输出：
+//[null, null, null, 2, 2, false]
+//
+//解释：
+//MyStack myStack = new MyStack();
+//myStack.push(1);
+//myStack.push(2);
+//myStack.top(); // 返回 2
+//myStack.pop(); // 返回 2
+//myStack.empty(); // 返回 False
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= x <= 9
+// 最多调用100 次 push、pop、top 和 empty
+// 每次调用 pop 和 top 都保证栈不为空
+//
+//
+//
+//
+// 进阶：你能否仅用一个队列来实现栈。
+//
+// Related Topics 栈 设计 队列 👍 855 👎 0
+
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Solution0225
+    {
+        private class MyStack
+        {
+
+            private Queue<int> _queue1 = new Queue<int>();
+            private Queue<int> _queue2 = new Queue<int>();
+
+            public MyStack() {
+
+            }
+
+            public void Push(int x) {
+                _queue2.Enqueue(x);
+                CopyTo(_queue1, _queue2);
+                Swap();
+            }
+
+            public int Pop() {
+                return _queue1.Dequeue();
+            }
+
+            public int Top() {
+                return _queue1.Peek();
+            }
+
+            public bool Empty() {
+                return _queue1.Count <= 0;
+            }
+
+            private void CopyTo(Queue<int> from, Queue<int> to) {
+                while (from.Count > 0) {
+                    to.Enqueue(from.Dequeue());
+                }
+            }
+
+            private void Swap() {
+                (_queue1, _queue2) = (_queue2, _queue1);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -15,6 +15,7 @@
 12. [x] [938. 二叉搜索树的范围和](https://leetcode.cn/problems/range-sum-of-bst/description/?envType=daily-question&envId=2024-02-26)
 13. [ ] [2867. 统计树中的合法路径数目](https://leetcode.cn/problems/count-valid-paths-in-a-tree/description/?envType=daily-question&envId=2024-02-27)
 14. [x] [2673. 使二叉树所有路径值相等的最小代价](https://leetcode.cn/problems/make-costs-of-paths-equal-in-a-binary-tree/description/?envType=daily-question&envId=2024-02-28)
+15. [x] [225. 用队列实现栈](https://leetcode.cn/problems/implement-stack-using-queues/description/?envType=daily-question&envId=2024-03-03)
 
 ## LeetCode 75
 
